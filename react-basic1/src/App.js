df2eefaae70013deececdb0e4e13b5d9afd1c2c7
Volodymyr <wolodymyrs@gmail.com>
2020-11-11 14:24:59 +0200

    check functionality added

--- react-basic1/src/App.js
+++ react-basic1/src/App.js
@@ -3,16 +3,27 @@ import TodoList from './todo/TodoList'
 
 function App() {
 
-  const todos = [
+  let [todos, setTodos] = React.useState([
     {id: 1, completed: false, title: 'To speak with develop manager'},
-    {id: 2, completed: false, title: 'To take a react course basic'},
+    {id: 2, completed: true, title: 'To take a react course basic'},
     {id: 3, completed: false, title: 'To swim'}
-  ]
+  ])
+
+  function toggleTodo(id) {
+    setTodos(
+      todos = todos.map(todo => {
+        if(todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    )
+  }
 
   return (
     <div className='wrapper'>
       <h1>REACT-training:  'Todo List'</h1>
-      <TodoList todos={todos}/>
+      <TodoList todos={todos} onToggle={toggleTodo} />
     </div>
 
     )
