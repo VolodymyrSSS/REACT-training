a32c302df6a6254ebd3b02eac9b3a93e9991b735
Volodymyr <wolodymyrs@gmail.com>
2020-11-11 22:12:01 +0200

    addNewTodo functionality

--- react-basic1/src/App.js
+++ react-basic1/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import TodoList from './todo/TodoList'
 import Context from './context'
+import AddTodo from './todo/AddTodo'
+
 
 function App() {
 
@@ -12,7 +14,7 @@ function App() {
 
   function toggleTodo(id) {
     setTodos(
-      todos = todos.map(todo => {
+      todos.map(todo => {
         if(todo.id === id) {
           todo.completed = !todo.completed
         }
@@ -25,14 +27,30 @@ function App() {
     setTodos(todos.filter(todo => todo.id !== id))
   }
 
+  function addTodo(title) {
+    setTodos(todos.concat([
+        {
+          title: title,
+          id: Date.now(),
+          completed: false
+        }
+      ])
+    )
+  }
+
   return (
     <Context.Provider value={{ removeTodo: removeTodo }}>
       <div className='wrapper'>
         <h1>REACT-training:  'Todo List'</h1>
-        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo} /> : <p>You have no current todos</p>}
+        <AddTodo onCreate={addTodo} />
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggleTodo} />
+        ) : (
+          <p>You have no current todos</p>
+        )}
       </div>
     </Context.Provider>
-    )
+  )
 }
 
-export default App;
+export default App
