6f8ba1da55c7df3c55467d06f6ff7b71d8367858
Volodymyr <wolodymyrs@gmail.com>
2021-02-16 12:03:36 +0200

    onChange, useState hook, dinamicaly add class

--- todo-minin/src/App.js
+++ todo-minin/src/App.js
@@ -3,16 +3,28 @@ import React from 'react';
 import TodoList from './Todo/TodoList';
 
 function App() {
-  const todos = [
+  const [todos, setTodos] = React.useState([
     {id:1, completed:false, title:'To buy a bread and butter'},
-    {id:2, completed:false, title:'To buy the fruits and vegetables'},
+    {id:2, completed:true, title:'To buy the fruits and vegetables'},
     {id:3, completed:false, title:'To buy a BMW X4'},
     {id:4, completed:false, title:'To buy a yacht "Beneteau Oceanis40Poole Length12.15"'}
-  ]
+  ]);
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if(todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    )
+  }
+
   return (
     <div className='wrapper'>
       <h1>React tutorial todo list</h1>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onToggle={toggleTodo} />
     </div>
   );
 }
