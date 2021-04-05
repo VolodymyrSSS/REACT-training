0b37708d1e6fca13a3baad1f7afff8d3a443481d
Volodymyr <wolodymyrs@gmail.com>
2021-04-05 12:38:43 +0300

    remove task, add task, toggle

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,9 +1,55 @@
+import { useState } from 'react';
 import Header from './components/Header';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'physical exercises: jogging, pullups, stretching',
+      day: 'March 25th, at 10.30am',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'IT-classes: unit-testing in React topic',
+      day: 'March 26th, at 13.45am',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: 'dacha activity - protect trees against-insect spray, book shelf constructions',
+      day: 'March 27th, at 12.10am',
+      reminder: false
+    }
+  ]);
+
+  // add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+
+    const newTask = {id, ...task};
+    setTasks([...tasks, newTask]);
+  }
+
+  // delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  };
+
+  // toggle remainder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task));
+  };
+
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
+      {tasks.length > 0 ? 
+        <Tasks  tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 
+        <h3>No tasks to show</h3>}
     </div>
   );
 }
---
+++ task-tracker/src/components/Tasks.js
@@ -0,0 +1,19 @@
+import Task from './Task';
+
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+  return (
+    <>
+      {tasks.map((task) => {
+        // return <h3 key={task.id}>{task.text}</h3>
+        return <Task 
+          key={task.id} 
+          task={task} 
+          onDelete={onDelete} 
+          onToggle={onToggle}
+        />
+      })}
+    </>
+  )
+}
+
+export default Tasks;
