5025e292a9b9feb79dd929d6280100de1e1525c1
Volodymyr <wolodymyrs@gmail.com>
2021-04-06 13:19:41 +0300

    update task with fake JSON-server

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -42,7 +42,6 @@ const App = () => {
   const fetchTasks = async () => {
     const res = await fetch('http://localhost:5000/tasks');
     const data = await res.json();
-
     return data;
   }
 
@@ -72,9 +71,25 @@ const App = () => {
     setTasks(tasks.filter((task) => task.id !== id)) // delete from UI
   };
 
+  // fetch single task
+  const fetchOneTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  }
+
   // toggle remainder
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task));
+  const toggleReminder = async (id) => {
+    const oneTaskToToggle = await fetchOneTask(id);
+    const updOneTask = {...oneTaskToToggle, reminder: !oneTaskToToggle.reminder} // create a new task and put it in a variable
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify(updOneTask)
+    });
+    const data = await res.json();
+    // setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task)); // toggle from UI
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder } : task));
   };
 
   return (
