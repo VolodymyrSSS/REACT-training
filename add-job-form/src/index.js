d7f67ca964952341fc7719c74523e4d04e695ffb
Volodymyr Sych <volodymyr_sych@epam.com>
2023-01-04 20:58:35 +0200

    publish the project

--- add-job-form/src/index.js
+++ add-job-form/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
-import App from './Api'; // a little test to pull in data from an API(Wikipedia API with URL endpoint as ["Seona Dancing",["Seona Dancing"],[""],["https://en.wikipedia.org/wiki/Seona_Dancing"]])
+import App from './App';
+// import App from './Api'; // a little test to pull in data from an API(Wikipedia API with URL endpoint as ["Seona Dancing",["Seona Dancing"],[""],["https://en.wikipedia.org/wiki/Seona_Dancing"]])
 import './index.css';
 
 // render the App to the root
