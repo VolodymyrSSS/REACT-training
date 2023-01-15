4379dc5eb20f17088e6e66ad5b8200b759ae1c49
Volodymyr Sych <volodymyr_sych@epam.com>
2023-01-15 13:40:06 +0200

    focus back to input field

--- groceries-list/src/AddItem.js
+++ groceries-list/src/AddItem.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { FaPlus } from 'react-icons/fa';
 
 const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
+  const inputRef = useRef();
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <label htmlFor="add-item">Add Item</label>
       <input
         autoFocus
+        ref={inputRef}
         id="add-item"
         type="text"
         placeholder="Add Item"
@@ -14,7 +16,11 @@ const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
         value={newItem}
         onChange={(e) => setNewItem(e.target.value)}
       />
-      <button type="submit" aria-label="Add Item">
+      <button
+        type="submit"
+        aria-label="Add Item"
+        onClick={() => inputRef.current.focus()}
+      >
         <FaPlus />
       </button>
     </form>
