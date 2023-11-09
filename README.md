d74aeb018103fb0a6492f7ab638fc16e7b8979f3
Volodymyr Sych <volodymyr_sych@epam.com>
2023-11-09 07:55:46 +0200

    minor fix

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # REACT-learning
-<<<<<<< HEAD
-The set of exercises to create sites with **REACT**
-=======
+
 The set of exercises to create sites using **REACT**
->>>>>>> 412d8ff202d24141ceffdfed6666e0d34c2cab5d
+
+# To start the project
+
+npm start
+
+# To start JSON server to work with local REST API
+
+npx json-server -p 3500 -w data/db.json
--- groceries-list/src/Footer.js
+++ groceries-list/src/Footer.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const Footer = ({ length }) => {
-  const today = new Date();
-  return (
-    <footer>
-      <p>
-        {length} list {length === 1 ? 'item' : 'items'}
-      </p>
-      <p>Copyright &copy; {today.getFullYear()}</p>
-    </footer>
-  );
+	const today = new Date();
+	return (
+		<footer>
+			<p>
+				{length} list {length === 1 ? 'item' : 'items'}
+			</p>
+			<p>Copyright &copy; {today.getFullYear()} Created by Volodymyr Sych</p>
+		</footer>
+	);
 };
 
 export default Footer;
