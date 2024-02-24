0bc6ad7a7228815d2529ed4a5fcdacd74ee6af54
Volodymyr Sych <volodymyr_sych@epam.com>
2024-02-24 15:34:28 +0200

    display email via local storage

--- functionalAproach/jwt-coocie-auth/src/components/shared/AuthContext.js
+++ functionalAproach/jwt-coocie-auth/src/components/shared/AuthContext.js
@@ -1,17 +1,34 @@
-import { createContext } from "react";
+import { useState, createContext } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext({});
 
 export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(() => {
+    let userProfile = localStorage.getItem("userProfile");
+    if (userProfile) {
+      return JSON.parse(userProfile);
+    }
+    return null;
+  });
+
+  const navigate = useNavigate();
 
   const loginApiCall = async (payload) => {
     await axios.post("http://localhost:4000/auth/login", payload, {
       withCredentials: true,
     });
+
+    const apiResponse = await axios.get("http://localhost:4000/user-profile", {
+      withCredentials: true,
+    });
+    setUser(apiResponse.data);
+    localStorage.setItem("userProfile", JSON.stringify(apiResponse.data));
+    navigate("/home");
   };
 
-  return <AuthContext.Provider value={{ loginApiCall }}>
+  return <AuthContext.Provider value={{ loginApiCall, user }}>
     { children }
   </AuthContext.Provider>
 }
--- functionalAproach/jwt-coocie-auth/src/components/shared/Layout.js
+++ functionalAproach/jwt-coocie-auth/src/components/shared/Layout.js
@@ -1,14 +1,24 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom' 
 import Navbar from 'react-bootstrap/Navbar';
 import { Container, Nav } from 'react-bootstrap';
+import AuthContext from './AuthContext';
 
 const Layout = ({ children }) => {
+
+  const {user} = useContext(AuthContext);
+
   return (
     <>
       <Navbar bg="primary" variant="dark">
         <Navbar.Brand as={Link} to="/home">JWT httpOnly Cookie Auth Login example</Navbar.Brand>
         <Nav className="ms-auto">
-          <Nav.Link as={Link} to="/login">Login</Nav.Link>
+          {user && (
+            <Nav.Link>{user.email}</Nav.Link>
+          )}
+          {!user && (
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+          )}
         </Nav>
       </Navbar>
       <Container>{ children }</Container>
