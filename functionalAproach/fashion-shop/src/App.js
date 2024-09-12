f9ae2443e4ae835d493916eec5e821390bb83894
Volodymyr Sych <volodymyr_sych@epam.com>
2024-09-12 07:49:18 +0300

    Fashion shop

---
+++ functionalAproach/fashion-shop/src/App.js
@@ -0,0 +1,17 @@
+import Arrivals from './components/arrivals/Arrivals.jsx';
+import Brands from './components/brands/Brands.jsx';
+import Header from './components/header/Header.jsx';
+import Promo from './components/promo/Promo.jsx';
+
+function App() {
+	return (
+		<div>
+			<Header />
+			<Promo />
+			<Brands />
+			<Arrivals />
+		</div>
+	);
+}
+
+export default App;
